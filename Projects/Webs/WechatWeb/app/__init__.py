29ccec24cad618005428fa970ac6c3e6fc5e7e97
patchlion <zxfd1434@sina.com>
2017-09-19 20:40:16 +0800

    添加微信公众号工程

---
+++ Projects/Webs/WechatWeb/app/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from wechat_sdk import WechatConf
+from wechat_sdk import WechatBasic
+
+conf = WechatConf(appid='wx2c21f552e41d70e3', token="diornext", appsecret="f8dc86b8b03aff989311a8650a1f81e1")
+wechat = WechatBasic(conf=conf)
+
+app = Flask(__name__)
+
+from app import views
