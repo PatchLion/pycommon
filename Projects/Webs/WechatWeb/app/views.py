412ba5d17b00fcd5641a84deaa9b777832a159ff
PatchLion <74740716@qq.com>
2017-09-19 23:24:09 +0800

    add /wechat/check

--- Projects/Webs/WechatWeb/app/views.py
+++ Projects/Webs/WechatWeb/app/views.py
@@ -4,8 +4,16 @@ from flask import request
 @app.route('/')
 @app.route('/index')
 def index():
+    return "欢迎光临狮子的小窝"
+
+@app.route('/wechat/check')
+def wechat_check():
     signature = request.args.get("signature")
     timestamp = request.args.get("timestamp")
     nonce = request.args.get("nonce")
+    echostr = request.args.get("echostr")
 
-    return wechat.check_signature(signature, timestamp, nonce)
+    if wechat.check_signature(signature, timestamp, nonce):
+        return echostr
+    else:
+        return "failed"
--- Projects/Webs/WechatWeb/run.py
+++ Projects/Webs/WechatWeb/run.py
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 
 from app import app
-app.run(host='0.0.0.0', debug=True, port=443)
+app.run(host='0.0.0.0', debug=True, port=80)
