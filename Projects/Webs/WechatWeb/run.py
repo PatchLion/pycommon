3519292cbb454c697bac456373efb50719bfa270
PatchLion <74740716@qq.com>
2017-09-20 00:07:30 +0800

    测试菜单

--- Projects/Webs/WechatWeb/run.py
+++ Projects/Webs/WechatWeb/run.py
@@ -2,4 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from app import app
+from app import wechat
+
+menu = {
+     "button":[
+     {
+          "type":"click",
+          "name":"注册",
+          "key":"REGEDIT"
+      },
+     {
+          "type":"click",
+          "name":"登录",
+          "key":"LOGIN"
+      }]
+}
+
+wechat.create_menu(menu)
+
 app.run(host='0.0.0.0', debug=True, port=80)
