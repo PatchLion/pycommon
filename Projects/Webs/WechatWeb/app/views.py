897f0fb9380e788bdd28e86bbbbb0f26a63e3485
PatchLion <74740716@qq.com>
2017-09-19 23:51:30 +0800

    自动回复消息

--- Projects/Webs/WechatWeb/app/views.py
+++ Projects/Webs/WechatWeb/app/views.py
@@ -6,14 +6,19 @@ from flask import request
 def index():
     return "欢迎光临狮子的小窝"
 
-@app.route('/wechat/check')
+@app.route('/wechat/check', methods=['GET', 'POST'])
 def wechat_check():
-    signature = request.args.get("signature")
-    timestamp = request.args.get("timestamp")
-    nonce = request.args.get("nonce")
-    echostr = request.args.get("echostr")
-
-    if wechat.check_signature(signature, timestamp, nonce):
-        return echostr
+    if request.method == 'POST':
+        wechat.parse_data(request.data, msg_signature=None, timestamp=None, nonce=None)
+        content = wechat.message.content  # 对应于 XML 中的 Content
+        wechat.response_text("你说的是:" + content, escape=False)
     else:
-        return "failed"
+        signature = request.args.get("signature")
+        timestamp = request.args.get("timestamp")
+        nonce = request.args.get("nonce")
+        echostr = request.args.get("echostr")
+
+        if wechat.check_signature(signature, timestamp, nonce):
+            return echostr
+        else:
+            return "failed"
